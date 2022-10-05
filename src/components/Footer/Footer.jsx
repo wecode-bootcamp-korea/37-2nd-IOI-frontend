8c139ac36d98080cdec0f0b1c2bd0f07a1e28220
Hyerin Joo <candy980426@gmail.com>
2022-10-05 21:25:24 +0900

    Add : Footer 레이아웃 구현

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,133 @@
 import React from 'react';
+import styled from 'styled-components';
+import { INFO_LIST, SOCIAL_LIST } from './infoData';
 
-function Footer(props) {
-  return <div>Footer</div>;
+function Footer() {
+  return (
+    <FooterContainer>
+      <ContentContainer>
+        <ContentLargeBox>
+          <ContentTitle>IOI</ContentTitle>
+          <Content>
+            IOI는 모든 사람이 사랑하는 일을 하며 살 수 있도록 세상을 바꾸고자
+            합니다.
+          </Content>
+          <Content>KR 한국어</Content>
+        </ContentLargeBox>
+        <ContentBox>
+          <ContentTitle>About</ContentTitle>
+          <Content>홈</Content>
+          <Content>기업교육</Content>
+          <Content>채용</Content>
+          <Content>도움말</Content>
+        </ContentBox>
+        <ContentBox>
+          <ContentTitle>크리에이터</ContentTitle>
+          <Content>크리에이터 센터</Content>
+          <Content>정규 클래스 지원하기</Content>
+          <Content>도움말</Content>
+        </ContentBox>
+        <ContentBox>
+          <ContentTitle>고객센터</ContentTitle>
+          <CallBtn>문의하기</CallBtn>
+          <Content>오전 10시 ~ 오후 6시 (주말, 공휴일 제외)</Content>
+        </ContentBox>
+      </ContentContainer>
+      <ContentContainer>
+        <SocialList>
+          {SOCIAL_LIST.map(social => (
+            <DetailBox key={social.id}>
+              <Detail>{social.title}</Detail>
+              <span>&middot;</span>
+            </DetailBox>
+          ))}
+        </SocialList>
+        <ListContainer>
+          <ListBox>
+            {INFO_LIST.map(info => (
+              <DetailBox key={info.id}>
+                <Detail>{info.title}</Detail>
+                <span>&middot;</span>
+              </DetailBox>
+            ))}
+          </ListBox>
+          <Detail>
+            주식회사 IOI | 대표 아이오아이 | 서울특별시 강남구 101 101-101
+            101층(아이오아이 타워) | 사업자등록번호 : 000-00-00000 |
+            통신판매업신고 : 0000-서울강남-0000
+          </Detail>
+        </ListContainer>
+      </ContentContainer>
+    </FooterContainer>
+  );
 }
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  border-top: 1px solid #e8e8e8;
+`;
+
+const ContentContainer = styled.div`
+  ${props => props.theme.variables.flex('row', 'center', 'flex-start')}
+  padding: 40px 0 64px 0;
+  width: 100%;
+`;
+
+const ContentLargeBox = styled.div`
+  width: 398px;
+`;
+
+const ContentBox = styled.div`
+  width: 240px;
+`;
+
+const ContentTitle = styled.p`
+  font-weight: bold;
+`;
+
+const Content = styled.p`
+  margin-top: 20px;
+  width: 260px;
+  font-size: 14px;
+  line-height: 18px;
+  color: #666666;
+`;
+
+const CallBtn = styled.button`
+  margin-top: 20px;
+  width: 250px;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
+  background-color: #f8f8f8;
+  font-weight: bold;
+`;
+
+const DetailBox = styled.div`
+  display: inline-block;
+`;
+
+const SocialList = styled.div`
+  width: 398px;
+  padding-right: 40px;
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 748px;
+`;
+
+const Detail = styled.span`
+  padding: 5px;
+  font-size: 12px;
+  line-height: 18px;
+  color: #a2a2a2;
+`;
+
+const ListBox = styled.div`
+  width: 748px;
+  padding-bottom: 10px;
+`;
 
 export default Footer;
---
+++ src/components/Footer/infoData.jsx
@@ -0,0 +1,105 @@
+export const INFO_LIST = [
+  {
+    id: '0',
+    title: '이용약관',
+    url: '#',
+  },
+  {
+    id: '1',
+    title: '개인정보 처리방침',
+    url: '#',
+  },
+  {
+    id: '2',
+    title: 'IOI프라임 이용약관',
+    url: '#',
+  },
+  {
+    id: '3',
+    title: '기프트카드 및 캐시 이용약관',
+    url: '#',
+  },
+  {
+    id: '4',
+    title: '환불 정책',
+    url: '#',
+  },
+  {
+    id: '5',
+    title: '사업자 정보 확인',
+    url: '#',
+  },
+  {
+    id: '6',
+    title: '단체/기업 교육 문의',
+    url: '#',
+  },
+  {
+    id: '7',
+    title: '제휴/협력 문의',
+    url: '#',
+  },
+  {
+    id: '8',
+    title: 'PR 관련 문의',
+    url: '#',
+  },
+  {
+    id: '9',
+    title: '지식재산권 침해 신고 센터',
+    url: '#',
+  },
+];
+
+export const SOCIAL_LIST = [
+  {
+    id: '0',
+    title: 'IOI Korea',
+    url: '#',
+  },
+  {
+    id: '1',
+    title: 'IOI USA',
+    url: '#',
+  },
+  {
+    id: '2',
+    title: 'IOI Japan',
+    url: '#',
+  },
+  {
+    id: '3',
+    title: 'Instagram',
+    url: '#',
+  },
+  {
+    id: '4',
+    title: 'Youtube',
+    url: '#',
+  },
+  {
+    id: '5',
+    title: 'Facebook',
+    url: '#',
+  },
+  {
+    id: '6',
+    title: 'Naverpost',
+    url: '#',
+  },
+  {
+    id: '7',
+    title: 'Naverblog',
+    url: '#',
+  },
+  {
+    id: '8',
+    title: 'Playstore',
+    url: '#',
+  },
+  {
+    id: '9',
+    title: 'Appstore',
+    url: '#',
+  },
+];
